82ae48fd42e6938d457bd8457763564649d8ec1f
agrinpc <agrinpc@gmail.com>
2022-12-13 16:39:33 +0100

    v 0.3

--- philo/main.c
+++ philo/main.c
@@ -6,31 +6,76 @@
 /*   By: miahmadi <miahmadi@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 17:19:26 by miahmadi          #+#    #+#             */
-/*   Updated: 2022/12/11 20:19:43 by miahmadi         ###   ########.fr       */
+/*   Updated: 2022/12/13 01:58:46 by miahmadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./philo.h"
 
-pthread_mutex_t	mutex;
-
-void	*eat(void *data)
+void	print_forks(t_data data)
 {
-	t_data 			*d;
+	int	i;
+
+	i = -1;
+	printf("\n");
+	while (++i < data.num_philos)
+		printf("Fork number %d : %d\n", i, data.forks[i].is_available);
+}
+
+void	*eat(void *philo)
+{
+	t_philo 		*d;
 	struct timeval	t;
 	long long		dt;
-	int				id;
 
-	d = data;
-	pthread_mutex_lock(&mutex);
-	id = d->curPhilo;
-	gettimeofday(&t, NULL);
-	dt = t.tv_sec * 1000 + t.tv_usec / 1000 - d->philos[id].last_meal;
-	if ( dt > 6000)
-		d->dead = 1;
-	printf("Philo Number %d ==> ID: %d, Status: %d, dt = %lld, Thread : %ld\n", id, d->philos[id].id, d->dead, dt, pthread_self());
-	sleep(1);
-	pthread_mutex_unlock(&mutex);
+	d = philo;
+	while (d->state < 4 && !d->data->dead)
+	{
+		pthread_mutex_lock(&d->data->general);
+		// print_forks(*(d->data));
+		gettimeofday(&t, NULL);
+		dt = t.tv_sec * 1000 + t.tv_usec / 1000;
+		if ( dt - d->last_meal > d->data->ttd)
+		{
+			d->data->dead = 1;
+			d->state = 4;
+			printf("%lld %d is dead\n", dt - d->data->start, d->id);
+			pthread_mutex_unlock(&d->data->general);
+			return (d);
+		}
+		if (d->left_fork->is_available && d->right_fork->is_available && d->state == 2)
+		{
+			d->state = 0;
+			d->right_fork->is_available = 0;
+			d->left_fork->is_available = 0;
+			pthread_mutex_lock(&(d->right_fork->fork));
+			pthread_mutex_lock(&(d->left_fork->fork));
+			pthread_mutex_unlock(&d->data->general);
+			gettimeofday(&t, NULL);
+			printf("%lld %d is eating\n", dt - d->data->start, d->id);
+			d->last_meal = dt;
+			usleep(d->data->tte * 1000);
+			d->right_fork->is_available = 1;
+			d->left_fork->is_available = 1;
+			pthread_mutex_unlock(&(d->right_fork->fork));
+			pthread_mutex_unlock(&(d->left_fork->fork));
+		}
+		else if (d->state < 4)
+		{
+			pthread_mutex_unlock(&d->data->general);
+			if (d->state == 0)
+			{
+				printf("%lld %d is sleeping\n", dt - d->data->start, d->id);
+				usleep(d->data->tts * 1000);
+				d->state = 1;
+			}
+			else if (d->state == 1)
+			{
+				printf("%lld %d is thinking\n", dt - d->data->start, d->id);
+				d->state = 2;
+			}
+		}
+	}
 	return (d);
 }
 
@@ -39,7 +84,7 @@ int	main(int argc, char **argv)
 	t_data		data;
 	int			i;
 	t_philo		*philos;
-	pthread_mutex_t *forks;
+	t_fork		*forks;
 	struct timeval t1;
 
 	if (argc !=5 && argc != 6)
@@ -51,44 +96,44 @@ int	main(int argc, char **argv)
 	data.ttd = atoi(argv[2]);
 	data.tte = atoi(argv[3]);
 	data.tts = atoi(argv[4]);
-	pthread_mutex_init(&mutex, NULL);
+	pthread_mutex_init(&data.general, NULL);
 	pthread_mutex_init(&(data.general), NULL);
+	gettimeofday(&t1, NULL);
+	data.start = t1.tv_sec * 1000 + t1.tv_usec / 1000;
 	data.dead = 0;
 	if (argc == 6)
 		data.num_eat = atoi(argv[5]);
 	philos = malloc(sizeof(t_philo) * data.num_philos);
-	forks = malloc(sizeof(pthread_mutex_t) * data.num_philos);
-	i = -1;
-	while (++i < data.num_philos)
-		pthread_mutex_init(&forks[i], NULL);
+	forks = malloc(sizeof(t_fork) * data.num_philos);
 	i = -1;
 	while (++i < data.num_philos)
 	{
-		gettimeofday(&t1, NULL);
-		philos[i].id = i + 1;
-		philos[i].last_meal = t1.tv_sec * 1000 + t1.tv_usec / 1000;
-		philos[i].right_fork = forks[i];
-		if (i == data.num_philos - 1)
-			philos[i].left_fork = forks[0];
-		else
-			philos[i].left_fork = forks[i + 1];
+		pthread_mutex_init(&(forks[i].fork), NULL);
+		forks[i].is_available = 1;
 	}
-	data.philos = philos;
-	while (!data.dead)
+	data.forks = forks;
+	i = -1;
+	while (++i < data.num_philos)
 	{
-		i = -1;
-		while (++i < data.num_philos)
-		{
-			data.curPhilo = i;
-			printf("%d\n",data.curPhilo);
-			pthread_create(&(philos[i].thread), NULL, &eat, &data);
-		}
-		i = -1;
-		while (++i < data.num_philos)
-		{
-			pthread_join(philos[i].thread, NULL);
-		}
-		
+		philos[i].id = i + 1;
+		philos[i].state = 2;
+		philos[i].last_meal = data.start;
+		philos[i].right_fork = &forks[i];
+		philos[i].data = &data;
+		if (i == data.num_philos - 1)
+			philos[i].left_fork = &forks[0];
+		else
+			philos[i].left_fork = &forks[i + 1];
+	}
+	i = -1;
+	while (++i < data.num_philos)
+	{
+		pthread_create(&(philos[i].thread), NULL, &eat, &philos[i]);
+	}
+	i = -1;
+	while (++i < data.num_philos)
+	{
+		pthread_join(philos[i].thread, NULL);
 	}
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: miahmadi <miahmadi@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 17:19:22 by miahmadi          #+#    #+#             */
-/*   Updated: 2022/12/11 20:10:03 by miahmadi         ###   ########.fr       */
+/*   Updated: 2022/12/13 01:42:21 by miahmadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,34 @@
 # include <pthread.h>
 # include <sys/time.h>
 
-
-typedef struct s_philo
+typedef struct s_fork
 {
-	int				id;
-	int				state;
-	long long		last_meal;
-	pthread_mutex_t	left_fork;
-	pthread_mutex_t	right_fork;
-	pthread_t		thread;
-}	t_philo;
+	pthread_mutex_t fork;
+	int				is_available;
+}				t_fork;
 
-
-typedef struct s_data
+typedef struct	s_data
 {
-	int		tte;
-	int		ttd;
-	int		tts;
-	int		num_philos;
-	int		num_eat;
-	int		dead;
-	t_philo	*philos;
-	int		curPhilo;
+	int				tte;
+	int				ttd;
+	int				tts;
+	int				num_philos;
+	int				num_eat;
+	int				dead;
+	t_fork			*forks;
+	long long		start;
 	pthread_mutex_t	general;
-} 	t_data;
+} 				t_data;
+
+typedef struct	s_philo
+{
+	int			id;
+	int			state;
+	long long	last_meal;
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+	pthread_t	thread;
+	t_data		*data;
+}				t_philo;
 
 #endif
